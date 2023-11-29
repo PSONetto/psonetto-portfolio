624f7a0265a32789916b04454d0f61e8e0cf664e
Plínio Netto <plinio.ice.pns@gmail.com>
2023-11-29 20:03:07 -0300

    fix: add images path to next-config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: [
+    {
+      protocol: 'http',
+      hostname: 'localhost',
+      port: '3000',
+      pathname: '/images/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'https://psonetto-portfolio.vercel.app',
+      port: '',
+      pathname: '/assets/**',
+    },
+  ],
+};
 
 module.exports = nextConfig;
