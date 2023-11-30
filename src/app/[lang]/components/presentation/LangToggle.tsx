4921c8d84bab880b2122a90663e7c5f802688d60
Plínio Netto <plinio.ice.pns@gmail.com>
2023-11-29 21:52:57 -0300

    feat: adds aria label to social media links

--- src/app/[lang]/components/presentation/LangToggle.tsx
+++ src/app/[lang]/components/presentation/LangToggle.tsx
@@ -8,13 +8,16 @@ export interface ILangToggleProps {
 
 export default function LangToggle({ lang }: ILangToggleProps) {
   let label: string;
+  let aria: string;
 
   switch (lang) {
     case 'pt':
       label = 'EN';
+      aria = 'Mudar idioma para Inglês';
       break;
     case 'en':
       label = 'PT';
+      aria = 'Change language to Portuguese';
       break;
     default:
       label = 'PT';
@@ -23,6 +26,7 @@ export default function LangToggle({ lang }: ILangToggleProps) {
     <Link
       href={`/${label.toLowerCase()}`}
       className="text-lg rounded p-2 bg-slate-900 transition ease-in-out duration-500 hover:scale-105"
+      aria-label=""
     >
       {label}
     </Link>
